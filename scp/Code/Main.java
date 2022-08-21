5a3a863aff35a3eea0dce03e482eb8af39f2bdf0
Papyoz <111503973+Papyoz@users.noreply.github.com>
2022-08-21 22:22:19 +0300

    Update Main.java

--- scp/Code/Main.java
+++ scp/Code/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+package Code;
+
+public class Main {
+    public static void main(String[] args){
+        recursive(1);
+    }
+    static void recursive(int  a){
+        System.out.println("Вверх " + a);
+        if (a<5) recursive(a + 1);
+        System.out.println("Вниз " + a);
+    }
 }
+/*
+Вверх 1
+Вверх 2
+Вверх 3
+Вверх 4
+Вверх 5
+Вниз 5
+Вниз 4
+Вниз 3
+Вниз 2
+Вниз 1
+
+Принцип Метод выполняется в методе,
+каждый из которых ожидает завершения следующего метода
+ */
